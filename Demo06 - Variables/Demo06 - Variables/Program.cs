0aa28696d94539fdb99bfe68533656c81c9b6d6f
Hossein Ahmadi <h.ahmadi@live.com>
2022-08-18 19:00:06 +0430

    Add demo 06 - variables

---
+++ Demo06 - Variables/Demo06 - Variables/Program.cs
@@ -0,0 +1,41 @@
+﻿// [type] [name];
+// [type] [name] = [value];
+
+// single variable declaration
+string myName = "My name is Hossein Ahmadi";
+// myName = "Hossein Ahmadi";
+// myName = 12; Cannot assign integer to string variables
+
+// single line multi variable declartion
+string name1 = "Welcome", name2, name3;
+
+Console.WriteLine("Hello!");
+Console.WriteLine("Welcome to TOSINSO");
+Console.WriteLine("------------------");
+Console.WriteLine(myName);
+Console.WriteLine(myName);
+Console.WriteLine(name1);
+
+
+Console.Beep();
+Console.ReadLine();
+
+
+// integer varibales
+
+sbyte number1 = -50;
+byte number2 = 255;
+// byte number3 = -30; cannot assign signed values to byte data type
+
+
+Console.WriteLine(number1);
+Console.WriteLine(number2);
+
+// decimal variables
+
+// int number3 = 52.6; cannot assign decimal numbers to integer data types
+
+double number4 = 52.6;
+
+Console.WriteLine(number4);
+decimal number5 = 52.6m;
