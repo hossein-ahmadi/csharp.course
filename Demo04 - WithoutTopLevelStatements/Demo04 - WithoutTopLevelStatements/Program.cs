4e229db8740a2294a4c45fab87a3be3dc1a43ba6
Hossein Ahmadi <h.ahmadi@live.com>
2022-08-18 17:59:07 +0430

    Add: Demo04 and Demo05

---
+++ Demo04 - WithoutTopLevelStatements/Demo04 - WithoutTopLevelStatements/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo04___WithoutTopLevelStatements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to C# Master class!");
+        }
+    }
+}
---
+++ Demo05 - ConsoleApplications/Demo05 - ConsoleApplications/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * TOSINSO
+ * CSharp Master Course
+ * More about console applications
+ * By: Hossein Ahmadi
+ */
+Console.WriteLine("Hello, World!"); // Display output to console window
+Console.WriteLine("---------------------------");
+Console.Write("Welcome ");
+Console.Write("To ");
+Console.WriteLine("C# Master Class!");
+Console.Beep();
+Console.ReadKey();
+Console.WriteLine("Instructor: Hossein Ahmadi");
