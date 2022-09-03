7f608e29371e8bec6032e89be3548e4f93256196
Hossein Ahmadi <h.ahmadi@live.com>
2022-09-03 17:40:00 +0430

    Add Demo09 - InputFromConsole

---
+++ Demo09 - InputFromConsole/Demo09 - InputFromConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Your firstname: ");
+var firstName = Console.ReadLine();
+Console.Write("Your lastname: ");
+var lastName = Console.ReadLine();
+
+
+var message = $"Hello {firstName} {lastName}, Welcome to C# Master Class!";
+
+Console.WriteLine(message);
+
+Console.WriteLine("----------------------------------");
+
+// int number1 = Console.ReadLine(); => Raise an error
+
+Console.Write("First number: ");
+string number1 = Console.ReadLine();
+Console.Write("Second number: ");
+string number2 = Console.ReadLine();
+
+var sum = number1 + number2;
+
+Console.WriteLine($"Sum of {number1} and {number2} is {sum}");
