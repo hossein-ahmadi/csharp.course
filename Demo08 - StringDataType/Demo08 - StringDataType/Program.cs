cb1c891960a199862b6aeffe24d15dc4788049e2
Hossein Ahmadi <h.ahmadi@live.com>
2022-09-03 16:50:56 +0430

    Add Demo08 - StringDataType

---
+++ Demo08 - StringDataType/Demo08 - StringDataType/Program.cs
@@ -0,0 +1,21 @@
+﻿string firstName = "Hossein";
+string lastName = "Ahmadi";
+
+var fullName = firstName + " " + lastName;
+
+var message1 = "Hello " + firstName + " " + lastName + ", Welcome to C# Master class!";
+
+// String interpolation
+var message2 = $"Helllo {firstName} {lastName}, Welcome to C# Master class!";
+
+Console.WriteLine(fullName);
+Console.WriteLine(message1);
+Console.WriteLine(message2);
+
+
+// Escapte characters
+var message3 = "My Course is \"C# MasterClass\"";
+var columns = "A\tB\tC\tD\r\nE\tF\tG\tH\t";
+
+Console.WriteLine(message3);
+Console.WriteLine(columns);
