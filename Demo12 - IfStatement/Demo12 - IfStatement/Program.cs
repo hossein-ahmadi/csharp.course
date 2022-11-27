9296446ebbc0619bca5f053d610c7ead9cdca6e4
Hossein Ahmadi <h.ahmadi@live.com>
2022-11-27 20:23:34 +0330

    Add Demo12 and Demo 13

---
+++ Demo12 - IfStatement/Demo12 - IfStatement/Program.cs
@@ -0,0 +1,23 @@
+﻿// If Statement
+
+Console.Write("Username: ");
+var username = Console.ReadLine();
+Console.Write("Password: ");
+var password = Console.ReadLine();
+
+if (username == "tosinso" && password == "123")
+{
+    Console.WriteLine("Welcome to my application");
+}
+else if (username == "hossein" && password == "789")
+{
+    Console.WriteLine("Welcome Hossein...");
+}
+else if (username == "ali" && password == "741")
+{
+    Console.WriteLine("Welcome Ali...");
+}
+else
+{
+    Console.WriteLine("Sorry! Username or password is invalid...");
+}
---
+++ Demo13 - SimpleCalculator/Demo13 - SimpleCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿// Simple calculator
+
+Console.WriteLine("Welcome to TOSINSO Calculator");
+Console.WriteLine("1. Add");
+Console.WriteLine("2. Subtract");
+Console.WriteLine("3. Multiply");
+Console.WriteLine("4. Divide");
+Console.WriteLine();
+Console.Write("Please choose an option: ");
+var option = Console.ReadLine();
+
+if (option != "1" && option != "2" && option != "3" && option != "4")
+{
+    Console.WriteLine("You selected invalid option!");
+    return;
+}
+
+Console.Write("First number: ");
+var number1 = int.Parse(Console.ReadLine());
+Console.Write("Second number: ");
+var number2 = int.Parse(Console.ReadLine());
+
+if (option == "1")
+{
+    Console.WriteLine($"{number1} + {number2} = {number1 + number2}");
+}
+else if (option == "2")
+{
+    Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+}
+else if (option == "3")
+{
+    Console.WriteLine($"{number1} * {number2} = {number1 * number2}");
+}
+else if (option == "4")
+{
+    if (number2 == 0)
+    {
+        Console.WriteLine("Cannot divide by zero!");
+        return;
+    }
+    Console.WriteLine($"{number1} / {number2} = {number1 / number2}");
+}
