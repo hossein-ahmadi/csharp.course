9794babbbf89db00e395146927b7d4891065ccb0
Hossein Ahmadi <h.ahmadi@live.com>
2022-11-28 16:42:02 +0330

    Add Demo14 - SwitchStatement

---
+++ Demo14 - SwitchStatement/Demo14 - SwitchStatement/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+ *  Demo14 - Switch statement
+ *  CSharp Master Class (TOSINSO)
+ *  Hossein Ahmadi
+ */
+
+Console.Write("Enter a character: ");
+var option = Console.ReadLine();
+
+switch (option)
+{
+    case "A":
+    case "a":
+        Console.WriteLine("You entered character a");
+        break;
+    case "B":
+    case "b":
+        Console.WriteLine("You entered character b");
+        break;
+    default:
+        Console.WriteLine("You entered other characters!");
+        break;
+}
+
+if (option == "a" || option == "A")
+{
+    Console.WriteLine("You enter character a");
+}
+else if (option == "b" || option == "B")
+{
+    Console.WriteLine("You enter character b");
+}
+else
+{
+    Console.WriteLine("You entered other characters");
+}
