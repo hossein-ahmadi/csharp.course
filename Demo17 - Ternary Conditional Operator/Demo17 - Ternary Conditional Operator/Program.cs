9ce8b1021845670e22f4386b0ac78ec58f457e61
Hossein Ahmadi <h.ahmadi@live.com>
2022-11-29 11:40:07 +0330

    Add Demo17 - Ternary Conditional Operator

---
+++ Demo17 - Ternary Conditional Operator/Demo17 - Ternary Conditional Operator/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ *  Demo17 - Ternary Conditional Operator
+ *  CSharp Master Class (TOSINSO)
+ *  Hossein Ahmadi
+ */
+
+Console.Write("Enter a number: ");
+var inputIsNumber = int.TryParse(Console.ReadLine(), out var number);
+if (!inputIsNumber)
+{
+    Console.WriteLine("Input is not in valid format!");
+    return;
+}
+
+var message = number % 2 == 0 ? "Number is even!" : "Number is odd!";
+Console.WriteLine(message);
+
+// number % 2 == 0 ? Console.WriteLine("Number is even!") : Console.WriteLine("Number is odd!"); => invalid syntax
