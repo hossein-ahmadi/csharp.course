1de56336518463b91eb69f4dd258c90cc8a6d25b
Hossein Ahmadi <h.ahmadi@live.com>
2022-12-01 15:47:38 +0330

    Add Demo23 - GotoStatement

---
+++ Demo23 - GotoStatement/Demo23 - GotoStatement/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ *  Demo23 - Goto Statement
+ *  CSharp Master Class (TOSINSO)
+ *  Hossein Ahmadi
+ */
+
+Console.WriteLine("Printing numbers between 1 to 10...");
+
+int counter = 1;
+
+START:
+if (counter > 10)
+{
+    goto END;
+}
+Console.WriteLine(counter);
+counter++;
+goto START;
+
+END:
+return;
