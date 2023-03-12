0a376a09e76a70b593b30c8bff7bed35b7d82b0c
Hossein Ahmadi <h.ahmadi@live.com>
2023-03-12 19:35:38 +0330

    Add Demo31 - MethodParams and Demo32 - MethodsRefOutInParameters

---
+++ Demo31 - MethodParams/Demo31 - MethodParams/Program.cs
@@ -0,0 +1,43 @@
+﻿// Method params
+
+Console.WriteLine(Sum(4,5,6));
+
+
+int Sum(params int[] numbers)
+{
+    int result = 0;
+
+    foreach (var number in numbers)
+    {
+        result += number;
+    }
+
+    return result;
+}
+
+Console.WriteLine(Calculate("sum", 2, 3, 4));
+Console.WriteLine(Calculate("average", 2, 3, 4));
+
+// decimal Calculate(params int[] numbers, string type) => invalid usage of params
+
+decimal Calculate(string type, params int[] numbers)
+{
+    decimal sum = 0m;
+
+    foreach (var number in numbers)
+    {
+        sum += number;
+    }
+
+    switch (type)
+    {
+        case "sum":
+        case "SUM":
+            return sum;
+        case "average":
+        case "AVERAGE":
+            return sum / numbers.Length;
+        default:
+            return 0;
+    }
+}
---
+++ Demo32 - MethodsRefOutInParameters/Demo32 - MethodsRefOutInParameters/Program.cs
@@ -0,0 +1,43 @@
+﻿// Methods ref, out and in
+int value = 8;
+
+ChangeValue(ref value);
+
+// ChangeValue(ref 12); => Uncomment to get error
+
+Console.WriteLine(value);
+
+void ChangeValue(ref int number)
+{
+    number = 12;
+}
+
+// out parameters
+
+int reminder;
+var result = Divide(15, 2, out reminder);
+
+var result2 = Divide(15, 2, out int remain);
+
+Console.WriteLine(result);
+Console.WriteLine(reminder);
+Console.WriteLine(remain);
+
+Console.WriteLine(Divide(20, 2, out _));
+
+//Console.WriteLine(Divide(15, 2));
+
+
+int Divide(int number1, int number2, out int reminder)
+{
+    reminder = number1 % number2;
+    return number1 / number2;
+}
+
+// in parameters
+
+int Sum(in int number1, int number2)
+{
+    // number1 = 9; => Uncomment to get error
+    return number1 + number2;
+}
