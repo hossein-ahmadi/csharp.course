a0724423f93cecf5ce0908cb2763b2003e13e74a
Hossein Ahmadi <h.ahmadi@live.com>
2023-04-10 15:38:21 +0430

    Update complementary section

--- Demo35 - MethodsComplementary/Demo35 - MethodsComplementary/Program.cs
+++ Demo35 - MethodsComplementary/Demo35 - MethodsComplementary/Program.cs
@@ -1,7 +1,43 @@
-﻿// Methods Complementary
+﻿// Complementary (local refs, ref return, expression bodies)
 
-// Local refs
+// local refs
 
-// Ref returns
+var numbers = new[] {1,2,3,4,5};
 
-// Expression Bodies
+ref var number = ref numbers[2];
+
+number = 12;
+
+Console.WriteLine(numbers[2]);
+
+// ref return
+
+var names = new[] { "Hossein", "Mohammad", "Alireza", "Mehdi" };
+
+ref var name = ref Find(names, "Alireza");
+
+Console.WriteLine(name);
+
+name = "Ali";
+
+ref string Find(string[] names, string key)
+{
+    for (int index = 0; index < names.Length; index++)
+    {
+        if (names[index] == key)
+            return ref names[index];
+    }
+
+    throw new InvalidOperationException();
+}
+
+// Expression bodies
+
+Console.WriteLine(Sum(12, 2));
+
+int Sum(int a, int b) => a + b;
+
+// int Sum(int a, int b)
+// {
+//     return a + b;
+// }
