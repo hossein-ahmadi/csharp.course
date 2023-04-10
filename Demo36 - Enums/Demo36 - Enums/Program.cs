e448a5772afaaf5c65c2c6d87c5ecf28207cfa29
Hossein Ahmadi <h.ahmadi@live.com>
2023-04-10 16:52:41 +0430

    Add enums demo project

---
+++ Demo36 - Enums/Demo36 - Enums/Program.cs
@@ -0,0 +1,25 @@
+﻿// Enum types
+
+Seasons s = Seasons.Autumn;
+
+Seasons s2 = (Seasons)40;
+
+Console.WriteLine(s);
+
+Console.WriteLine((int)s);
+
+Console.WriteLine(s2);
+
+enum Seasons : byte
+{
+    Spring = 10,
+    Summer = 20,
+    Autumn = 30,
+    Winter = 40
+}
+
+enum Sex : byte
+{
+    Male = 1,
+    Female = 2
+}
