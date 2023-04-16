d6321df7f32b058fdff8c38f902fa66a36b64cc5
Hossein Ahmadi <h.ahmadi@live.com>
2023-04-16 17:11:13 +0430

    Add Demo37 - ClassAndObject

---
+++ Demo37 - ClassAndObject/Demo37 - ClassAndObject/Program.cs
@@ -0,0 +1,25 @@
+﻿// Class and Object
+
+Car car = new Car();
+car.Color = "Red";
+car.Model = "BMW";
+
+Console.WriteLine(car.Model);
+car.Start();
+car.Park();
+
+class Car
+{
+    public string Model;
+    public string Color;
+
+    public void Start()
+    {
+        Console.WriteLine("Car started...");
+    }
+
+    public void Park()
+    {
+        Console.WriteLine("Car parked...");
+    }
+}
