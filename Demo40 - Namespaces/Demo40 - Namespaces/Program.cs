b73e17f7e6acf6d7fd7eef11ce15c39472c03dd5
Hossein Ahmadi <h.ahmadi@live.com>
2023-05-06 15:28:00 +0430

    Add Demo40 - Namespaces

---
+++ Demo40 - Namespaces/Demo40 - Namespaces/Program.cs
@@ -0,0 +1,7 @@
+﻿using Demos.Demo40_Namespaces.Tools;
+
+// Demo40 - Namespaces
+
+var messages = new Messages();
+
+messages.Welcome("Hossein Ahmadi");
---
+++ Demo40 - Namespaces/Demo40 - Namespaces/Tools/Math.cs
@@ -0,0 +1,20 @@
+﻿// namespace Demos.Demo40_Namespaces.Tools
+// {
+        // public class Math
+        // {
+        //
+        // }
+// }
+
+namespace Demos.Demo40_Namespaces.Tools;
+
+public class Math
+{
+    public int Sum(int x, int y) => x + y;
+    public int Divide(int x, int y) => x / y;
+}
+
+public class Messages
+{
+    public void Welcome(string name) => Console.WriteLine($"Welcome dear {name}");
+}
