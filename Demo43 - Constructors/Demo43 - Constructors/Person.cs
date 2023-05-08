163a9d98f8ea4a5a7960f52f96ff50351bc122d1
Hossein Ahmadi <h.ahmadi@live.com>
2023-05-08 20:58:43 +0430

    Add Demo43 - Constructors

---
+++ Demo43 - Constructors/Demo43 - Constructors/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Demo43___Constructors;
+
+public class Person
+{
+    public Person(){}
+
+    public Person(string firstname, string lastname, int age)
+    {
+        if (firstname.Length > 20)
+            throw new ArgumentException();
+        this.Firstname = firstname;
+        this.Lastname = lastname;
+        this.Age = age;
+    }
+
+    public Person(string firstname, string lastname) : this(firstname, lastname, 0)
+    {
+    }
+
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ Demo43 - Constructors/Demo43 - Constructors/Program.cs
@@ -0,0 +1,14 @@
+﻿// Demo42 - Constructors
+
+using Demo43___Constructors;
+
+var p1 = new Person()
+{
+    Firstname = "Hossein",
+    Lastname = "Ahmadi",
+    Age = 37
+};
+
+var p2 = new Person("Mohammad", "Nasiri", 37);
+
+var p3 = new Person("Ali", "Shokrollahi");
