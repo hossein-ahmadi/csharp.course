291909070f9b71cd7a55430252237290520972d0
Hossein Ahmadi <h.ahmadi@live.com>
2023-05-09 13:55:57 +0430

    Add Demo44 - ReadonlyKeyword

---
+++ Demo44 - ReadonlyKeyword/Demo44 - ReadonlyKeyword/Person.cs
@@ -0,0 +1,29 @@
+﻿namespace Demo44___ReadonlyKeyword;
+
+public class Person
+{
+    // Fields
+    private readonly int age;
+
+    // Constructors
+
+    public Person(int age, string firstname, string lastname)
+    {
+        this.age = age;
+        Firstname = firstname;
+        Lastname = lastname;
+    }
+
+    // Properties
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+
+    // Methods
+
+    public int Age => age;
+
+    // public void ChangeAge(int value)
+    // {
+    //     this.age = value;
+    // }
+}
---
+++ Demo44 - ReadonlyKeyword/Demo44 - ReadonlyKeyword/Program.cs
@@ -0,0 +1,7 @@
+﻿// Demo44 - Readonly keyword
+
+using Demo44___ReadonlyKeyword;
+
+var p1 = new Person(37,"Hossein","Ahmadi");
+
+Console.WriteLine(p1.Age);
