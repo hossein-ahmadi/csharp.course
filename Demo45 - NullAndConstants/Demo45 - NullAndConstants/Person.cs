952f946fa4e0d501d911b185c7c58785e076eace
Hossein Ahmadi <h.ahmadi@live.com>
2023-05-21 16:11:07 +0430

    Add Demo45 - NullAndConstants

---
+++ Demo45 - NullAndConstants/Demo45 - NullAndConstants/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo45___NullAndConstants;
+
+public class Person
+{
+    public const int FixedValue = 12;
+
+    public string Name { get; set; }
+}
---
+++ Demo45 - NullAndConstants/Demo45 - NullAndConstants/Program.cs
@@ -0,0 +1,38 @@
+﻿// Demo45 - NullAndConstants
+
+using Demo45___NullAndConstants;
+
+Person p = null;
+
+// This line raise NullReferenceException
+// Console.WriteLine(p.Name);
+
+if (p != null)
+{
+    Console.WriteLine(p.Name);
+}
+
+// Reference nullable types
+Person? p2 = null;
+
+// int number = null;
+int? number = null;
+if (number != null)
+{
+    Console.WriteLine(number);
+}
+int? number2 = 12;
+
+if (number2.HasValue)
+{
+    Console.WriteLine(number2);
+}
+
+// Constants
+const int fixedNumber = 12; // Constants require initial value
+
+// Cannot change value of constant variables. Uncomment to raise exception
+// fixedNumber = 5;
+
+
+
