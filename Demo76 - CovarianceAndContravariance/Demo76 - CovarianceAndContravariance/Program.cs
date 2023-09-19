66775116563c0b82949620913e21635e378967a1
Hossein Ahmadi <h.ahmadi@live.com>
2023-09-19 18:53:34 +0330

    Add Demo76 - CovarianceAndContravariance and Demo77 - AnonymousTypes

---
+++ Demo76 - CovarianceAndContravariance/Demo76 - CovarianceAndContravariance/Program.cs
@@ -0,0 +1,50 @@
+﻿// Demo76 - Covariance and Contravariance
+// TOSINSO C# Mega Course
+
+
+// Covariance sample
+IBase<string> strBase = new Child<string>();
+
+IBase<object> objBase = strBase;
+
+IEnumerable<string> strEnumerable = new List<string>();
+
+IEnumerable<object> objEnumerable = strEnumerable;
+
+// Contravariance sample
+IBase2<object> objBase2 = new Child2<object>();
+
+IBase2<string> strBase2 = objBase2;
+
+Action<object> objAction = obj =>
+{
+    Console.WriteLine(obj?.ToString());
+};
+
+Action<string> strAction = objAction;
+
+public interface IBase<out T>
+{
+    public T Dosomething();
+}
+
+public class Child<T> : IBase<T>
+{
+    public T Dosomething()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IBase2<in T>
+{
+    public void Domesomething(T input)
+    {
+
+    }
+}
+
+public class Child2<T> : IBase2<T>
+{
+
+}
---
+++ Demo77 - AnonymousTypes/Demo77 - AnonymousTypes/Program.cs
@@ -0,0 +1,24 @@
+﻿// Demo77 - Anonymous types
+// TOSINSO C# Mega Course
+
+var student = new
+{
+    Id = 1,
+    Firstname = "Hossein",
+    Lastname = "Ahmadi",
+    Address = new
+    {
+        State = "Alborz",
+        City = "Karaj"
+    }
+};
+
+// student.Firstname = "test"; not allowed
+var student2 = student with
+{
+    Firstname = "Mehdi",
+    Lastname = "Adeli",
+    // Age = 12 adding new properties not allowed
+};
+
+Console.WriteLine(student2);
