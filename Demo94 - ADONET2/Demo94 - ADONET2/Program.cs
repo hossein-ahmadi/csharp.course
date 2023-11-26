9c1929f5cd956c0cd06181e4cc6da8e77dc06d6f
Hossein Ahmadi <h.ahmadi@live.com>
2023-11-26 20:01:03 +0330

    Add Demo94 - ADONET2

---
+++ Demo94 - ADONET2/Demo94 - ADONET2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Demo94 - ADO.NET Part 2
+// TOSINSO C# Mega Course
+
+using Microsoft.Data.SqlClient;
+
+using var connection = new SqlConnection("Data Source=.; Initial Catalog=AccountingDb; Integrated Security=true; TrustServerCertificate=true");
+
+Console.Write("Username: ");
+var username = Console.ReadLine();
+Console.Write("Password: ");
+var password = Console.ReadLine();
+
+
+var query = $"SELECT COUNT(*) FROM Users WHERE Username = @p1 and Password = @p2";
+
+var usernameParameter = new SqlParameter("p1", username);
+var passwordParameter = new SqlParameter("p2", password);
+var command = new SqlCommand(query, connection);
+command.Parameters.Add(usernameParameter);
+command.Parameters.Add(passwordParameter);
+
+connection.Open();
+var usersCount = command.ExecuteScalar();
+
+connection.Open();
