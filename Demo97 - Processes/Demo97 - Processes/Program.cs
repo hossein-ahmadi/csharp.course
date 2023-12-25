f728a2fddcf373a2c93588fbf3c1602560a01d8b
Hossein Ahmadi <h.ahmadi@live.com>
2023-12-25 08:15:17 +0330

    Add Demo97 and Demo98

---
+++ Demo97 - Processes/Demo97 - Processes/Program.cs
@@ -0,0 +1,49 @@
+﻿// Demo97 - Processes
+// TOSINSO C# Mega Course
+
+// Get All Processes
+
+using System.Diagnostics;
+
+var allProcesses = Process.GetProcesses();
+
+foreach (var process in allProcesses)
+{
+    Console.WriteLine($"{process.Id}\t{process.ProcessName}");
+}
+
+// Get process by id
+
+var vsProcess = Process.GetProcessById(24328);
+
+Console.WriteLine("Display visual studio process info");
+Console.WriteLine("-------------------------------------");
+Console.WriteLine($"Id: {vsProcess.Id}");
+Console.WriteLine($"Name: {vsProcess.ProcessName}");
+foreach (ProcessThread thread in vsProcess.Threads)
+{
+    Console.Write($"\t{thread.Id}");
+}
+
+// var notepadProcess = new Process()
+// {
+//     StartInfo = new ProcessStartInfo("notepad","d:\\mytestfile.txt")
+// };
+//
+// notepadProcess.Start();
+//
+// var calcProcess = new Process()
+// {
+//     StartInfo = new ProcessStartInfo("calc")
+// };
+// calcProcess.Start();
+
+var edgeProcess = new Process()
+{
+    StartInfo = new ProcessStartInfo("msedge","tosinso.com")
+    {
+        UseShellExecute = true
+    },
+};
+
+edgeProcess.Start();
---
+++ Demo98 - AppDomains/Demo98 - AppDomains/Program.cs
@@ -0,0 +1,13 @@
+﻿// Demo98 - AppDomains
+// TOSINSO C# Mega Course
+
+var currentDomain = AppDomain.CurrentDomain;
+
+Console.WriteLine($"Id: {currentDomain.Id}");
+Console.WriteLine($"Name: {currentDomain.FriendlyName}");
+Console.WriteLine($"Path: {currentDomain.BaseDirectory}");
+
+foreach (var assembly in currentDomain.GetAssemblies())
+{
+    Console.WriteLine($"assembly: {assembly.FullName}");
+}
