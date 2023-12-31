24639c91c262d33e986ced6a02d5ff586a12227c
Hossein Ahmadi <h.ahmadi@live.com>
2023-12-31 19:30:04 +0330

    Add Demo99 - Threads

---
+++ Demo99 - Threads/Demo99 - Threads/Models/ThreadInputModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Demo99___Threads.Models;
+
+public class ThreadInputModel
+{
+    public ThreadInputModel(int from, int to)
+    {
+        From = from;
+        To = to;
+    }
+
+    public int From { get; set; }
+    public int To { get; set; }
+}
---
+++ Demo99 - Threads/Demo99 - Threads/Program.cs
@@ -0,0 +1,33 @@
+﻿// Demo99 - Threads
+// TOSINSO C# Mega Course
+
+using Demo99___Threads.Models;
+
+ThreadStart ts1 = () =>
+{
+    for (int counter = 0; counter < 100; counter++)
+    {
+        Console.WriteLine(counter);
+    }
+};
+
+ParameterizedThreadStart pts = input =>
+{
+    var tim = (ThreadInputModel)input;
+
+    for (int counter = tim.From; counter < tim.To; counter++)
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine(counter);
+    }
+};
+
+Thread thread1 = new Thread(ts1)
+{
+    Priority = ThreadPriority.Highest
+};
+Thread thread2 = new Thread(pts);
+
+thread1.Start();
+
+thread2.Start(new ThreadInputModel(200,300));
