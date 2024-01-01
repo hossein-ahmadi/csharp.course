84bcbc56cec15be69d3e38dbf13386b9d77cbaea
Hossein Ahmadi <h.ahmadi@live.com>
2024-01-01 14:25:08 +0330

    Add Demo102 and Demo103

---
+++ Demo102 - Timer/Demo102 - Timer/Program.cs
@@ -0,0 +1,10 @@
+﻿// Demo102 - Timer
+// TOSINSO C# Mega Course
+
+Timer timer = new Timer(state =>
+{
+    Console.WriteLine(DateTime.Now.ToString("hh:mm:ss"));
+},null,0,2000);
+
+Console.WriteLine("Press any key to exit!");
+Console.ReadKey();
---
+++ Demo103 - ThreadPools/Demo103 - ThreadPools/Program.cs
@@ -0,0 +1,13 @@
+﻿// Demo103 - Thread Pools
+// TOSINSO C# Mega Course
+
+ThreadPool.QueueUserWorkItem(state =>
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(i);
+    }
+});
+
+Console.WriteLine("Press any key to exit!");
+Console.ReadKey();
