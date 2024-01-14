37245768cd75a546392cdfc7146b81106919b017
Hossein Ahmadi <h.ahmadi@live.com>
2024-01-14 19:03:33 +0330

    Add Demo107

---
+++ Demo107 - HelloWPF/Demo107 - HelloWPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Demo107___HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Demo107 - HelloWPF/Demo107 - HelloWPF/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo107___HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ChangeTextButton_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayMessageTextBlock.Text = "Welcome to TOSINSO C# Mega Course";
+        }
+    }
+}
