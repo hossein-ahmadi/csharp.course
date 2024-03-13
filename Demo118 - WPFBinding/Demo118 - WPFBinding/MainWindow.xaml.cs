8b09209c1889d2986712d592e514d2dc2258c750
Hossein Ahmadi <h.ahmadi@live.com>
2024-03-13 19:52:59 +0330

    Add Demo118 - WPFBinding

---
+++ Demo118 - WPFBinding/Demo118 - WPFBinding/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Demo118___WPFBinding.Models;
+
+namespace Demo118___WPFBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Person person = new Person()
+        {
+            Firstname = "Hossein",
+            Lastname = "Ahmadi",
+            Email = "hossein@ahmadi.com"
+        };
+
+    public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void ShowNameButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"{person.Firstname} {person.Lastname}");
+        }
+    }
+}
---
+++ Demo118 - WPFBinding/Demo118 - WPFBinding/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace Demo118___WPFBinding.Models;
+
+public class Person
+{
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    public string Email { get; set; }
+}
