d1eaff268fe71e59007b9d2f1d96047388962fba
Hossein Ahmadi <h.ahmadi@live.com>
2024-06-23 15:50:08 +0330

    Add new projects

---
+++ Demo138 - WindowsIdentity/Demo138 - WindowsIdentity/Program.cs
@@ -0,0 +1,16 @@
+﻿// Demo138 - Windows Identity
+// TOSINSO C# Mega Course
+
+using System.Security.Principal;
+
+var windowsIdentity = WindowsIdentity.GetCurrent();
+
+var windowsPrincipal = new WindowsPrincipal(windowsIdentity);
+
+Console.WriteLine($"Name: {windowsIdentity.Name}");
+Console.WriteLine("-------------------------------------");
+foreach (var group in windowsIdentity.Groups)
+{
+    Console.WriteLine($"{group.Translate(typeof(NTAccount))}");
+}
+Console.WriteLine($"User is administrator: {windowsPrincipal.IsInRole("BUILTIN\\Administrators")}");
---
+++ Demo139 - Claims/Demo139 - Claims/Program.cs
@@ -0,0 +1,19 @@
+﻿// Demo139 - Claims
+// TOSINSO C# Mega Course
+
+using System.Security.Claims;
+
+ClaimsIdentity identity = new ClaimsIdentity();
+
+identity.AddClaim(new Claim(ClaimTypes.Name, "Hossein Ahmadi"));
+identity.AddClaim(new Claim(ClaimTypes.Role, "content-manager"));
+identity.AddClaim(new Claim(ClaimTypes.Email, "ahmadi@tosinso.com"));
+identity.AddClaim(new Claim("UserMobile", "+989121111111"));
+
+Console.WriteLine($"Name: {identity.Name}");
+
+ClaimsPrincipal principal = new ClaimsPrincipal(identity);
+
+Console.WriteLine($"Is in role Content Manager: {principal.IsInRole("content-manager")}");
+Console.WriteLine($"Identity email: {principal.FindFirst(ClaimTypes.Email)?.Value}");
+Console.WriteLine($"Identity mobile: {principal.FindFirst("UserMobile")?.Value}");
